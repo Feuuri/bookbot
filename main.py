a60b33f0f6eae2c3b9b32e28a3e6747c10269eca
Feuuri <macfeurstein@gmail.com>
2025-03-09 14:13:33 +0100

    Added function "count characters"

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import get_num_words, get_book_text
+from stats import get_num_words, get_book_text, get_num_characters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_characters = get_num_characters(text)
     print(f"{num_words} words found in the document")
+    print(num_characters)  # Print the dictionary directly
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,15 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_characters(text):
+    character_counts = {}
+    
+    for char in text.lower():  # Convert to lowercase here so every character is standardized
+        if char in character_counts:  # Check if the character is already in the dictionary
+            character_counts[char] += 1  # Increment the count
+        else:
+            character_counts[char] = 1  # Add the character with an initial count of 1
+
+    return character_counts
+
